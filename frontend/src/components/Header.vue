1ae6aaa0f4f2cbbe568d81cf8ece5932674dc05a
majorpeter <majorpeter29@gmail.com>
2023-06-06 18:05:08 +0200

    changed header link to router-link

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <router-link :to="{name: 'home'}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <span class="fs-4">snaphopper</span>
-        </a>
+        </router-link>
         <ul class="nav nav-pills">
             <li class="nav-item">
                 <router-link :to="{name: 'home'}" class="nav-link">Stacks</router-link>
