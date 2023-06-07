f0b59af10dd3cd91eacc459f58b4a8d2377907bc
majorpeter <majorpeter29@gmail.com>
2023-06-07 22:43:18 +0200

    trigger logout & login on token expiry (403 error)

--- frontend/src/services/ApiClient.ts
+++ frontend/src/services/ApiClient.ts
@@ -1,10 +1,24 @@
-import { store } from "@/store"
+import router, { paths } from "@/router";
+import { MutationTypes, store } from "@/store"
 import axios from "axios"
 
 export default () => {
-    return axios.create({
+    const instance = axios.create({
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }
     });
+
+    instance.interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+        // force logout & login if token expired
+        if (error.response.status == 403) {
+            store.commit(MutationTypes.logout);
+            router.push(paths.login);
+        }
+
+        return error;
+    });
+    return instance;
 }
