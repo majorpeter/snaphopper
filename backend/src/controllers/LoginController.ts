f686647ac3be47b6afdb3d23210bb248854820d3
majorpeter <majorpeter29@gmail.com>
2023-06-10 16:53:52 +0200

    implemented token generation & check for single admin user

--- backend/src/controllers/LoginController.ts
+++ backend/src/controllers/LoginController.ts
@@ -3,12 +3,43 @@ import { endpoints } from "../lib/api";
 import { Config } from "../lib/config";
 import bcrypt from 'bcryptjs';
 
+let validSessionToken: {
+    value: string;
+    timestamp: number;
+} | undefined = undefined;
+
+const session_timeout_ms = 15 * 60 * 1000;
+
+/**
+ * this allows a single admin user to be logged in for a limited time
+ * @note in this application, this is enough; json web token could be used for multi-user situations
+ * @param token session token from Authorization header
+ * @returns true if correct and still valid
+ */
+export function isTokenValid(token: string): boolean {
+    if (validSessionToken) {
+        if (validSessionToken.value == token) {
+            const age = new Date().getTime() - validSessionToken.timestamp;
+            if (age < session_timeout_ms) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 export default function(app: Express, config: Config.Type) {
     app.post<{}, endpoints.login.resp_type, endpoints.login.type>(endpoints.login.url, async (req, res) => {
         if (bcrypt.compareSync(req.body.password, config.login_password_hash!)) {
+            const ts = new Date().getTime();
+            validSessionToken = {
+                value: bcrypt.hashSync(ts.toString(), config.salt!),    // just something random
+                timestamp: ts
+            }
+
             res.send({
                 success: true,
-                token: 'mytoken',
+                token: validSessionToken.value,
             });
         } else {
             res.send({
--- backend/src/lib/policies.ts
+++ backend/src/lib/policies.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
+import { isTokenValid } from "../controllers/LoginController";
 
 export const authenticationRequred: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
-    if (req.header('Authorization') == 'Bearer mytoken') {
+    if (req.header('Authorization')?.startsWith('Bearer ') && isTokenValid(req.header('Authorization')!.substring('Bearer '.length))) {
         next();
     } else {
         res.sendStatus(403);
