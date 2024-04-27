3d3b62b516a402a77a1767e44d02b6e74da0c3fc
majorpeter <majorpeter29@gmail.com>
2024-04-27 13:22:34 +0200

    updated (c) year

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="py-3 my-4">
-        <p class="text-center text-body-secondary">© 2023
+        <p class="text-center text-body-secondary">© 2024
             <a href="https://github.com/majorpeter" target="_blank">majorpeter</a>
         </p>
     </footer>
